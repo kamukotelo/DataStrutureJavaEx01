ec3701ee8a4b01bea884660c00f450042e4f0d1d
kamukotelo <kamukotelo@gmail.com>
2022-04-13 11:06:52 +0100

    Primeiro commit

---
+++ Array01.java
@@ -0,0 +1,24 @@
+package class01;
+public class Array01 {
+    public static void main(Strings[] args){
+        int[] bigarray = new int[10];
+        bigarray[0]= 10;
+        bigarray[1]= 20;
+        bigarray[2]= 30;
+        bigarray[3]= 40;
+        bigarray[4]= 50;
+        bigarray[5]= 60;
+        bigarray[6]= 70;
+        bigarray[7]= 80;
+        bigarray[8]= 90;
+        bigarray[9]= 100;
+        bigarray[10]= 110;
+        System.out.println("Printing array datas");
+        for (int i=0; i<= bigarray.length;  i++){
+            System.out.println(bigarray[i]);
+        }
+        System.out.println("We wil do more in  the next class, thank you");
+    }
+
+
+}z
